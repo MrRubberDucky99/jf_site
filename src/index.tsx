3ae67b8c8f877a73f8cd21a06ed6c6ecdba4408e
Judah Fuller <judahfuller@gmail.com>
2022-01-11 07:48:09 +0000

    Add Fonts

--- src/index.tsx
+++ src/index.tsx
@@ -45,6 +45,22 @@ const theme = createTheme({
 			],
 		},
 	},
+	typography: {
+		fontFamily: [
+			"Ubuntu Mono",
+			"Roboto Mono",
+			"Roboto",
+			"-apple-system",
+			"BlinkMacSystemFont",
+			'"Segoe UI"',
+			'"Helvetica Neue"',
+			"Arial",
+			"sans-serif",
+			'"Apple Color Emoji"',
+			'"Segoe UI Emoji"',
+			'"Segoe UI Symbol"',
+		].join(","),
+	},
 });
 
 ReactDOM.render(
